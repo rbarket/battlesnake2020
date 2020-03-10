3c2661308fa310f697860eff20b9daece94e82ac
rbarket <barketrashid@hotmail.com>
2020-03-10 14:59:19 -0700

    made check function recursive to look n spaces ahead

--- app/moveScore.py
+++ app/moveScore.py
@@ -13,13 +13,13 @@ def scoreMove(moves, snakes, width, height):
 
 	for move, coord in moves.items(): 
 		if (move == 'down'):
-			scoreDict.update({'down': check(coord, snakeList, width, height)})
+			scoreDict.update({'down': check(coord, snakeList, width, height, n=2)})
 		if (move == 'up'):
-			scoreDict.update({'up': check(coord, snakeList, width, height)})
+			scoreDict.update({'up': check(coord, snakeList, width, height, n=2)})
 		if (move == 'right'):
-			scoreDict.update({'right': check(coord, snakeList, width, height)})
+			scoreDict.update({'right': check(coord, snakeList, width, height, n=2)})
 		if (move == 'left'):
-			scoreDict.update({'left': check(coord, snakeList, width, height)})
+			scoreDict.update({'left': check(coord, snakeList, width, height, n=2)})
 
 	max_val = max(scoreDict.values())
 	bestMove = [k for k, v in scoreDict.items() if v == max_val] # Note: will return a list with only 1 item
@@ -27,7 +27,7 @@ def scoreMove(moves, snakes, width, height):
 	return bestMove
 
 
-def check(move, occupied, width, height):
+def check(move, occupied, width, height, n=1):
 	"""
 	_Purpose:
 	takes a move coord and returns a score (max 4) based on how many free spaces are directly around the move
@@ -42,30 +42,28 @@ def check(move, occupied, width, height):
 	score (int): score of move
 	"""
 
-	def coordMaker(coord):
-		"""
-		_Purpose:
-		makes up down left right coordinates of the current coordinated passed to it
-		"""
 
-		up = (coord[0], coord[1]-1)
-		down = (coord[0], coord[1]+1)
-		left = (coord[0]-1, coord[1])
-		right = (coord[0]-1, coord[1])
-		spots = [up, down, left, right]
-		return spots
+	if (n==0): # base case
+		return 0	
+
+	up = (move[0], move[1]-1)
+	down = (move[0], move[1]+1)
+	left = (move[0]-1, move[1])
+	right = (move[0]+1, move[1])
 
 	score = 0
 
-	for pos in coordMaker(move):		
-		if not (( (pos[0]-1,pos[1]) in occupied) or (pos[0]-1 < 0)): #left
-			score += 1
-		if not (( (pos[0]+1,pos[1]) in occupied) or (pos[0]+1 > width-1)): # right
-			score += 1
-		if not (( (pos[0],pos[1]-1) in occupied) or (pos[1]-1 < 0)): # up
-			score += 1
-		if not (( (pos[0],pos[1]+1) in occupied) or (pos[1]+1 > height-1)): #down
-			score += 1
+	# for pos in coordMaker(move):
+
+
+	if not (( left in occupied) or (left[0] < 0)): #left
+		score += 1 + check(left, occupied, width, height, n-1)
+	if not (( right in occupied) or (right[0] > width-1)): # right
+		score += 1 + check(right, occupied, width, height, n-1)
+	if not (( up in occupied) or (up[1] < 0)): # up
+		score += 1 + check(up, occupied, width, height, n-1)
+	if not (( down in occupied) or (down[1] > height-1)): #down
+		score += 1 + check(down, occupied, width, height, n-1)
 	return score
 
 
