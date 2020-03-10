1c0e18e0e32867fdbae74fbb012b21ece4f0489c
Guillermo <christian.guillermo@ia.ca>
2020-03-10 13:27:51 -0700

    randomly pick attk vs defence when called

--- app/tailChase.py
+++ app/tailChase.py
@@ -1,4 +1,4 @@
-def action(moveList, occupiedList, head, tail, width, height):
+def action(moveList, enemyBodyList, head, tail, width, height, hp):
     """
     _Purpose: To chase my own tail
 
@@ -13,15 +13,22 @@ def action(moveList, occupiedList, head, tail, width, height):
     returnMove(string): Move needed in order to chase tail
     """
     # @TODO GET SNAKE TO ESCAPE IT'S OWN BODY WHEN IT ENCAPSULATE'S ITSELF
+    randMethod = {enemyChase(head, tail, moveList), myChase(head, enemyBodyList, enemyBodyList)}
+    return random.randMethod
+
+def myChase(head, tail, moveList):
     
-    # Initialize values
+    # Initialize Values
 
     moveToTail = 'up' # arbitrary value
     distance = abs(head[0] - tail[0]) + abs(head[1] - tail[1]) # calculate the distance between head and tail
     
     #Simply move towards the closest direction in order to follow the tail
+    
     for direction in moveList.items():
         if (direction < distance):
             movetoTail = direction
     return movetoTail
 
+def enemyChase(head, moveList, enemyBodyList):
+    # @TODO FUNCTION NOT CHANGED
